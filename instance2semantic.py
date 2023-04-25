329b1b060095b8668eef3519d32e8a1f9069c75f
Omar Alama <omaralama31@yahoo.com>
2023-04-25 19:16:37 -0400

    implemented default behavior for instance2semantic

--- instance2semantic.py
+++ instance2semantic.py
@@ -21,25 +21,17 @@ import numpy as np
 import argparse
 import os
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument("annot_path", help="The path to the coco annotations json file", type=str)
-    parser.add_argument("masks_path", help="The path to where to store the generated segmentation masks", type=str)
-    
-    args = parser.parse_args()
-    
-    coco=COCO(args.annot_path)
+def instance2semantic(annot_path, masks_path):
+    print(f"Converting instance annotations in {annot_path} to semantic segmentation masks stored in {masks_path} directory")
+    coco=COCO(annot_path)
     
     assert len(coco.getCatIds()) == 1 # This scripts only generates a binary mask
     
-    os.makedirs(args.masks_path, exist_ok=True)
+    os.makedirs(masks_path, exist_ok=True)
     img_ids = coco.getImgIds()
     for img_id in tqdm.tqdm(img_ids, total=len(img_ids)):
         img = coco.loadImgs([img_id])[0]
         mask = np.zeros(shape=(img["height"], img["width"]))
-        mask_pil = Image.fromarray(mask)
-        polygons = list()
         ann_ids = coco.getAnnIds(imgIds=img['id'])
         anns = coco.loadAnns(ann_ids)
         for ann in anns:
@@ -48,4 +40,22 @@ if __name__ == "__main__":
                 # We have to do it one by one otherwise nested polygons may be subtracted.
                 mask = cv2.fillPoly(mask, [poly], color=(255,255,255)) 
 
-        Image.fromarray((mask).astype("uint8")).save(os.path.join(args.masks_path, img["original_filename"]))
+        Image.fromarray((mask).astype("uint8")).save(os.path.join(masks_path, img["original_filename"]))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--annot_path", help="The path to the coco annotations json file", type=str, default=None)
+    parser.add_argument("--masks_path", help="The path to where to store the generated segmentation masks", type=str, default=None)
+    
+    args = parser.parse_args()
+
+    if args.annot_path is None or args.masks_path is None:
+        print("One or no arguments have been passed. Using default behavior which assumes dataset is in default location.")
+        instance2semantic("./dataset/livecell_coco_train.json", "./dataset/images/livecell_train_val_masks")
+        instance2semantic("./dataset/livecell_coco_val.json", "./dataset/images/livecell_train_val_masks")
+        instance2semantic("./dataset/livecell_coco_test.json", "./dataset/images/livecell_test_masks")
+    else:
+        instance2semantic(args.annot_path, args.masks_path)
+    
